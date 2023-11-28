25771bf7cc30bb6394f3ba0c7a0c1c0fbc32c084
R <yz10444@nyu.edu>
2023-11-28 20:49:36 +0800

    吖
    啊

--- proB/b.js
+++ proB/b.js
@@ -1,202 +1,268 @@
 let index = 0;
 let flowerImages = [];
-let bubbleImages=[]
-let sunflower
-let wogua
-let mic
-let d1
-let d2
-let d3
-let d4
-let d5
-let d6
-let zo1
-let zo2
+let bubbleImages = [];
+let zoImages=[];
+let sunflower;
+let wogua;
+let sheshou;
+let piano;
+let mic;
+let d1;
+let d2;
+let d3;
+let d4;
+let d5;
+let d6;
 let flowers = [];
-let bubbles=[]
+let bubbles = [];
+let zb
+let zos=[];
 let img;
 let bgm;
-let i
+let i;
 let drum;
 let drumbutton;
 let at;
-function preload(){
-  drum = loadImage("pb/32.png")
-  mic=loadImage("pb/2.png")
-  drumbutton=loadImage('R.jpg')
-  sunflower=loadImage('pb/17.gif')
-  img=loadImage("pb/1.jpg");
-  at=loadImage("B.jpg")
-  bgm=loadSound("bgm.ogg");
-  d1=loadSound("drum/d1.mp3");
-  d2=loadSound("drum/d2.mp3");
-  d3=loadSound("drum/d3.mp3");
-  d4=loadSound("drum/d4.mp3")
-  d5=loadSound("drum/d5.mp3")
-  d6=loadSound("drum/d6.mp3")
-  zo1=loadImage("")
-  // for(let i = 0; i < 9; i ++){
-  //   let url = "../PROB/flower/h" + parseInt(i+1) + ".png";
-  //   flowerImages.push(loadImage(url));
-  // }
-  // for(let i = 0; i < 7; i ++){
-  //   let url = "../PROB/bubble/h" + parseInt(i+1) + ".png";
-  //   flowerImages.push(loadImage(url));
-  // }
+function preload() {
+  drum = loadImage("pb/32.png");
+  piano=loadImage("pb/5.png")
+  sheshou=loadImage('pb/16.gif')
+  mic = loadImage("pb/2.png");
+  drumbutton = loadImage('R.jpg');
+  sunflower = loadImage('pb/17.gif');
+  img = loadImage("pb/1.jpg");
+  at = loadImage("B.jpg");
+  bgm = loadSound("bgm.ogg");
+  zb=loadImage('Z.jpg')
+  d1 = loadSound("drum/d1.mp3");
+  d2 = loadSound("drum/d2.mp3");
+  d3 = loadSound("drum/d3.mp3");
+  d4 = loadSound("drum/d4.mp3");
+  d5 = loadSound("drum/d5.mp3");
+  d6 = loadSound("drum/d6.mp3");
+  wogua=loadImage('pb/6.gif')
+  zo1=loadImage('pb/8.gif')
+  zo2=loadImage('pb/92.gif')
+  for (let i = 0; i < 9; i++) {
+    let url = "../flower/h" + parseInt(i + 1) + ".png";
+    flowerImages.push(loadImage(url));
+  }
+  for (let i = 0; i < 7; i++) {
+    let url = "../bubble/b" + parseInt(i + 1) + ".png";
+    bubbleImages.push(loadImage(url));
+  }
+  for (let i = 0; i < 4; i++) {
+    let url = "../cd/d" + parseInt(i + 1) + ".png";
+    cdImages.push(loadImage(url));
+  }
 }
 
 function setup() {
-  index=0
+  index = 0
   let canvas = createCanvas(1200, 750);
   // console.log(document.getElementsByClassName("bluebox"));
   canvas.parent("bluebox")
-    console.log(flowerImages);
-    
+  console.log(flowerImages);
+
 
 }
+
+
+
+
+
+function draw() {
+  console.log(index)
+  let x=100*noise(0.005*frameCount)
+  let y=100*noise(sin(frameCount * 0.01)+10000)
+
+  background(220)
+  image(img, 0, 0)
+  image(drumbutton, 10, 710)
+  image(at, 50, 710)
+  image(zb, 90, 710)
+  image(sunflower, 550, 440)
+  image(wogua, 660,300)
+  image(mic,560,350)
+  image(drum,700,350)
+ image(sheshou,300,450)
+
+ image(zo1,300+x,600-y)
+ image(zo1,400+x,550-y)
+ image(zo2,600,630)
+ push()
+translate(500,590)
+ rotate(noise(0.005*frameCount))
+ scale(noise(0.005*frameCount))
+ image(zo2,0,0)
+pop()
+ image(zo1,700,560)
+ image(zo1,800,570)
+ image(zo2,900,590)
+ image(zo2,1000,600)
+ image(zo2,200,600)
+ image(zo2,300,570)
+ push()
+ scale(0.8)
+  image(piano,420,500)
+  pop()
+
+  if (!bgm.isPlaying()) {
+    bgm.loop();
+  }
+  // image(drum, 0, 0);
+
+  for (let i = 0; i < flowers.length; i++) {
+    flowers[i].update()
+    flowers[i].display()
+    if (flowers[i].y > height) {
+      flowers.splice(i, 1);
+
+    }
+  }
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].update()
+    bubbles[i].display()
+    if (bubbles[i].y <0) {
+      bubbles.splice(i, 1);
+    }
+  }
+
   
 
+  // console.log(bgm.isPlaying);
+  if (keyIsPressed === true) {
+    if(index == 2){//撒花
+      if (key == "n") {
+   
+       for (let i = 0; i < 100; i++) {
+         flowers.push(new flower(random(0, width)));
+         console.log('testtesttest');
+       }
+     }
+      if (key == "b") {
+       for (let i = 0; i < 40; i++) {
+         bubbles.push(new bubble(random(0, width)));
+       }
+     }
+     if (key == "m") {
+      for (let i = 0; i < 20; i++) {
+        cds.push(new cd(random(0, height)));
+      }
+    }
+    } 
 
-
-
-function draw(){
-console.log(index)
-background(220)  
-image(img,0,0)
-image(drumbutton,10,710)
-image(at,50,710)
-image(sunflower,550,440)
-image(drum,550,800)
-image(mic,550,730)
-
-if(!bgm.isPlaying()){
-bgm.loop();
-}
-// image(drum, 0, 0);
-
-for(let i = 0; i < flowers.length; i ++){
-flowers[i].update()
-flowers[i].display()
-if(flowers[i].y > height){
-  flowers.splice(i, 1);
-
-}
-}
-for(let i = 0; i < bubbles.length; i ++){
-  bubbles[i].update()
-  bubbles[i].display()
+  }
 }
 
-// console.log(bgm.isPlaying);
-function keyPressed(){
- 
-  if(index=1){//敲鼓
-    if(key == "a"){
-      if(!d1.isPlaying()){
+
+function keyPressed() {
+  if (index == 1) {//敲鼓
+    if (key == "a") {
+      if (!d1.isPlaying()) {
         d1.play();
-        }
-      }
-      if(key == "s"){
-        if(!d2.isPlaying()){
-          d2.play();
-          }
-        }
-        if(key == "d"){
-          if(!d3.isPlaying()){
-            d3.play();
-            }
+        console.log('play1');
 
-            if(key == "f"){
-              if(!d4.isPlaying()){
-                d4.play();
-                }
-              }
-              if(key == "g"){
-                if(!d5.isPlaying()){
-                  d5.play();
-                  }
-                }
-                if(key == "h"){
-                  if(!d6.isPlaying()){
-                    d6.play();
-                    }
-                  }
-          }
-
-  }
-  if(index == 2){//撒花
-    if(key == "f"){
-      for(let i = 0; i < 100; i ++){
-        flowers.push(new flower(random(0, width)));
       }
     }
-  if(key=="b"){
-    for(let i = 0; i < 40; i ++){
-      bubbles.push(new bubble(random(0, width)));
+    if (key == "s") {
+      if (!d2.isPlaying()) {
+        d2.play();
+        console.log('play1');
+
+      }
+    }
+    if (key == "d") {
+      if (!d3.isPlaying()) {
+        d3.play();
+        console.log('play1');
+
+      }
     }
 
+    if (key == "f") {
+      if (!d4.isPlaying()) {
+        d4.play();
+          console.log('play1');
+
+      }
+    }
+    if (key == "g") {
+      if (!d5.isPlaying()) {
+        d5.play();
+        console.log('play2');
+
+      }
+    }
+    if (key == "h") {
+      if (!d6.isPlaying()) {
+        d6.play();
+        console.log('play3');
+
+      }
+    }
   }
 
-  }
-  
-}
 }
 
-function mouseClicked(){
-  if(mouseX < 40 && mouseX > 10 && mouseY > 710 && mouseY < 740){
+function mouseClicked() {
+  if (mouseX < 40 && mouseX > 10 && mouseY > 710 && mouseY < 740) {
     index = 1;
   }
-  if(mouseX < 80 && mouseX > 50 && mouseY > 710 && mouseY < 740){
+  if (mouseX < 80 && mouseX > 50 && mouseY > 710 && mouseY < 740) {
     index = 2;
   }
+  if (mouseX < 90 && mouseX > 120 && mouseY > 710 && mouseY < 740) {
+    index = 3;
+  }
 }
 
-class bubble{
-  constructor(x){
-    this.x =x;
-    this.y = random(30,700);
-    this.xspd =0;
+class bubble {
+  constructor(x) {
+    this.x = x;
+    this.y = random(750,1250);
+    this.xspd = 0;
     this.yspd = 0;
-    this.yA = random(0.05,0.1);
-    this.s=random(0.2,0.3)
+    this.yA = random(0.05, 0.1);
+    this.s = random(0.2, 0.3)
     this.rotateAng = 0;
     this.imageIndex = floor(random(0, 7));
-  
-    
+
+
   }
-    display(){
-      push();
-      translate(this.x, this.y);
-      scale(this.s);
-      rotate(this.rotateAng);
-      image(flowerImages[this.imageIndex], 0, 0);
-      pop();
-    }
-    update(){
-      this.xspd =map(noise(sin(frameCount * 0.01) ), 0 , 1, -0.1,0.1);
-      this.yspd += this.yA;
-      this.rotateAng = map(sin(frameCount * 0.01), -1, 1, 0, PI/6);
-      this.x += this.xspd;
-      this.y += this.yspd;
-    
-    }
+  display() {
+    push();
+    translate(this.x, this.y);
+    scale(this.s);
+    rotate(this.rotateAng);
+    image(bubbleImages[this.imageIndex], 0, 0);
+    pop();
+  }
+  update() {
+    this.xspd = map(noise(sin(frameCount * 0.01)), 0, 1, -0.1, 0.1);
+    this.yspd += this.yA;
+    this.rotateAng = map(sin(frameCount * 0.01), -1, 1, 0, PI / 6);
+    this.x += this.xspd;
+    this.y -= this.yspd;
+
+  }
 }
 
 
-class flower{
-  constructor(x){
-  this.x =x;
-  this.y = random(-500, 0);
-  this.xspd =0;
-  this.yspd = 0;
-  this.yA = random(0.05,0.1);
-  this.s=random(0.2,0.3)
-  this.rotateAng = 0;
-  this.imageIndex = floor(random(0, 7));
+class flower {
+  constructor(x) {
+    this.x = x;
+    this.y = random(-300, 0);
+    this.xspd = 0;
+    this.yspd = 0;
+    this.yA = random(0.1,0.2);
+    this.s = random(0.2, 0.3)
+    this.rotateAng = 0;
+    this.imageIndex = floor(random(0, 7));
 
-  
-}
-  display(){
+
+  }
+  display() {
     push();
     translate(this.x, this.y);
     scale(this.s);
@@ -204,14 +270,46 @@ class flower{
     image(flowerImages[this.imageIndex], 0, 0);
     pop();
   }
-  update(){
-    this.xspd =map(noise(sin(frameCount * 0.01) ), 0 , 1, -0.1,0.1);
+  update() {
+    this.xspd = map(noise(sin(frameCount * 0.01)), 0, 1, -0.1, 0.1);
     this.yspd += this.yA;
-    this.rotateAng = map(sin(frameCount * 0.01), -1, 1, 0, PI/6);
+    this.rotateAng = map(sin(frameCount * 0.01), -1, 1, 0, PI / 5);
     this.x += this.xspd;
     this.y += this.yspd;
-  
+
   }
-  
+
+}
+
+class cd {
+  constructor(y) {
+    this.x = random(-300, 0);
+    this.y = y;
+    this.xspd = 0;
+    this.yspd = 0;
+    this.yA = random(0.1,0.2);
+    this.s = random(0.2, 0.3)
+    this.rotateAng = 0;
+    this.imageIndex = floor(random(0, 7));
+
+
   }
-  
+  display() {
+    push();
+    translate(this.x, this.y);
+    scale(this.s);
+    rotate(this.rotateAng);
+    image(flowerImages[this.imageIndex], 0, 0);
+    pop();
+  }
+  update() {
+    this.xspd = map(noise(sin(frameCount * 0.01)), 0, 1, -0.1, 0.1);
+    this.yspd += this.yA;
+    this.rotateAng = map(sin(frameCount * 0.01), -1, 1, 0, PI / 5);
+    this.x += this.xspd;
+    this.y += this.yspd;
+
+  }
+
+}
+
---
+++ proB/pac.js
@@ -0,0 +1,216 @@
+const totalBeans =25;
+const beansSize = 15;
+let player;
+let beans = [];
+let monsters = [];
+let monsterSize = 30;
+let score = 0;
+let isMouseClicked = false;
+let zo
+let sun
+let obstacles = [];
+let obstacleWidth = 250;
+const obstacleHeight = 20;
+
+function preload(){
+    zo=loadImage('Z.jpg')
+    sun=loadImage('pb/11.png')
+}
+function setup() {
+  // Create canvas and initialize objects
+  createCanvas(850,850);
+  player = {x: width / 2, y: height / 2, speed: 5, size: 30};
+
+  // Add beans
+  for (let i = 0; i < totalBeans; i++) {
+    beans.push({x: random(width - beansSize), y: random(height - beansSize)});
+  }
+
+  // Add monsters
+  for (let i = 0; i < 7; i++) {
+    monsters.push({x: random(width), y: random(height), speed: 2});
+  }
+
+  // Add obstacles
+  for (let i = 0; i < 10; i++) {
+    obstacles.push({x: random(width - obstacleWidth), y: random(height - obstacleHeight)});
+  }
+}
+
+function draw() {
+  background(220);
+  showPlayer();
+  fill(255)
+  textSize(20);
+  text("click to start", 370,420);
+  if (isMouseClicked === true){
+  movePlayer()
+}
+
+  if (isMouseClicked) {
+    for (let monster of monsters) {
+      moveMonster(monster);
+    }
+    for (let obstacle of obstacles) {
+      obstacle.x += random(-4, 4);
+      obstacle.y += random(-4, 4);
+    }
+  }
+
+  // Display beans
+  for (let bean of beans) {
+    showBean(bean);
+  }
+
+  // Display monsters
+  for (let monster of monsters) {
+    showMonster(monster);
+  }
+
+  // Display obstacles
+  for (let obstacle of obstacles) {
+    showObstacle(obstacle);
+  }
+
+  // Check collision
+  if (checkCollision()) {
+    gameOver();
+  }
+
+  // Eat beans
+  for (let i = beans.length - 1; i >= 0; i--) {
+    if (dist(player.x, player.y, beans[i].x, beans[i].y) < (player.size + beansSize) / 2) {
+      score += 1;
+      beans.splice(i, 1);
+      if (beans.length % 5 == 0) {
+        monsterSize += 15;
+        obstacleWidth += 10;
+        monsterSize = constrain(monsterSize, 30, 70);
+      }
+    }
+  }
+
+  // Collide with monsters
+  for (let i = monsters.length - 1; i >= 0; i--) {
+    if (dist(player.x, player.y, monsters[i].x, monsters[i].y) < monsterSize / 2) {
+      gameOver();
+    }
+  }
+
+  // Display score
+  textSize(32);
+  fill(0);
+  text("Score: " + score, 10, 30);
+
+  if (score === totalBeans) {
+    strokeweight(10)
+    textSize(50);
+    background(0, 0, 0, 0);
+    isMouseClicked = false;
+  }
+}
+
+function mouseClicked() {
+  isMouseClicked = true;
+}
+
+function showPlayer() {
+  fill(255, 0, 0);
+  ellipse(player.x, player.y, player.size);
+}
+
+function movePlayer() {
+  if (keyIsDown(UP_ARROW)) {
+    player.y -= player.speed;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    player.y += player.speed;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    player.x -= player.speed;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    player.x += player.speed;
+  }
+
+  player.x = constrain(player.x, 0, width);
+  player.y = constrain(player.y, 0, height);
+}
+
+function checkCollision() {
+  for (let obstacle of obstacles) {
+    if (
+      player.x + player.size / 2 > obstacle.x &&
+      player.x - player.size / 2 < obstacle.x + obstacleWidth &&
+      player.y + player.size / 2 > obstacle.y &&
+      player.y - player.size / 2 < obstacle.y + obstacleHeight
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function showBean(bean) {
+  scale(0.5)
+  image(sun,bean.x,bean.y)
+  
+}
+
+function showMonster(monster) {
+  fill(0, 0, 255);
+  ellipse(monster.x, monster.y, monsterSize);
+}
+
+function moveMonster(monster) {
+  monster.x += 4 * random(-monster.speed, monster.speed);
+  monster.y += 4 * random(-monster.speed, monster.speed);
+
+  monster.x = constrain(monster.x, 0, width);
+  monster.y = constrain(monster.y, 0, height);
+}
+
+function showObstacle(obstacle) {
+  fill(0);
+  rect(obstacle.x, obstacle.y, obstacleWidth, obstacleHeight);
+}
+
+function gameOver() {
+  noLoop();
+  background(0);
+  fill(255);
+  textAlign(CENTER);
+  textSize(30);
+  text("Game Over", width / 2, height / 2 + 30);
+  text("Restart: push SPACE to restart", width / 2, height / 2 - 30);
+}
+function keyPressed() {
+  if (keyCode === 32) { // 32 is the keycode for spacebar
+    resetGame();
+  }
+}
+
+function resetGame() {
+  isMouseClicked = false;
+ player.x = width / 2;
+  player.y = height / 2;
+monsterSize = 30
+  
+  score = 0;
+
+  beans = [];
+  for (let i = 0; i < totalBeans; i++) {
+    beans.push({x: random(width - beansSize), y: random(height - beansSize)});
+  }
+
+  monsters = [];
+  for (let i = 0; i < 7; i++) {
+    monsters.push({x: random(width), y: random(height), speed: 2});
+  }
+  obstacles = [];
+  for (let i = 0; i < 10; i++) {
+    obstacles.push({x: random(width - obstacleWidth), y: random(height - obstacleHeight)});
+  }
+  
+  loop();
+}
